f4abba4a824f7bfec8566e549d459e827c6159c1
eigil-lippert <32103662+eigil-lippert@users.noreply.github.com>
2017-10-28 15:23:23 +0200

    added linear interpolation and nan_helper function

--- helpers/data_analysis.py
+++ helpers/data_analysis.py
@@ -273,3 +273,23 @@ def visualization_PCA(eig_ratios):
     plt.gcf().set_size_inches(s, plt.gcf().get_size_inches()[1])
 
     plt.show()
+
+
+def nan_helper(y):
+    """Helper to handle indices and logical indices of NaNs.
+    Input:
+        - y, 1d numpy array with possible NaNs
+    Output:
+        - nans, logical indices of NaNs
+        - index, a function, with signature indices= index(logical_indices),
+          to convert logical indices of NaNs to 'equivalent' indices
+    """
+    return np.isnan(y), lambda z: z.nonzero()[0]
+
+
+def linear_interpolation(x):
+    '''linear interpolation of NaNs'''
+
+    nans, indices = nan_helper(x[:, 0])
+
+    x[: 0][nans]= np.interp(indices(nans), indices(~nans), x[:, 0][~nans])
